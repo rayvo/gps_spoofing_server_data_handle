678389bd23eab581989dcdc842c1cc240e422bd2
Taeyu <voquocduy1983@gmail.com>
2019-02-17 01:00:27 +0900

    change messages

--- location_data.php
+++ location_data.php
@@ -13,31 +13,32 @@ and open the template in the editor.
 
 
  
-    //m_log("TAEYU".PHP_EOL);
     //Make sure that it is a POST request.
     if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0){
-       // throw new Exception('Request method must be POST!');
-        print("The server is running");
-    }
-
-    //Make sure that the content type of the POST request has been set to application/json
-    $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
-    if(strcasecmp($contentType, 'application/json') != 0){
-        throw new Exception('Content type must be: application/json');
-    }
-
-    //Receive the RAW post data.
-    $content = trim(file_get_contents("php://input"));
-    //m_log("RAW DATA = $content".PHP_EOL);
-
-    //Attempt to decode the incoming RAW post data from JSON.
-    $decoded = json_decode($content, true); //JSON object
-
-    //If json_decode failed, the JSON is invalid.
-    if(!is_array($decoded)){
-        throw new Exception('Received content contained invalid JSON!');
-    }
-    //Process the JSON***********************
+        // throw new Exception('Request method must be POST!');
+         print("<h1 style='color: red; text-align:center'>Welcome to Our Request Handling Server</h1><br><h2 style='color: green'>THE SERVER IS RUNNING NOW...</h2><h3>You can use this url to send your data in JSON format to our server.</h3>");
+     }
+ 
+     //Make sure that the content type of the POST request has been set to application/json
+     $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+     if(strcasecmp($contentType, 'application/json') != 0){
+         //throw new Exception('Content type must be: application/json');
+         print("<h3>You can check your data using this <a href='index.php'>link</a></h3>");
+         exit();
+     }
+ 
+     //Receive the RAW post data.
+     $content = trim(file_get_contents("php://input"));
+     //m_log("RAW DATA = $content".PHP_EOL);
+ 
+     //Attempt to decode the incoming RAW post data from JSON.
+     $decoded = json_decode($content, true); //JSON object
+ 
+     //If json_decode failed, the JSON is invalid.
+     if(!is_array($decoded)){
+         //throw new Exception('Received content contained invalid JSON!');
+     }
+     //Process the JSON***********************
     $clubDevice = null;
     $clubDeviceId = null;
     foreach($decoded as $k=>$v) {
